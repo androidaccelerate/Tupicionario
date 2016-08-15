4450b424a81995d2197e74d1af9a4f8e314858d1
Fillipe Cordeiro <fillipe@androidpro.com.br>
2016-08-15 17:11:11 -0300

    Etapa 04

--- app/src/main/java/br/com/androidpro/tupicionario/MainActivity.java
+++ app/src/main/java/br/com/androidpro/tupicionario/MainActivity.java
@@ -1,7 +1,9 @@
 package br.com.androidpro.tupicionario;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvBichos = (TextView) findViewById(R.id.tvBichos);
+        tvBichos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        TextView tvCulinaria = (TextView) findViewById(R.id.tvCulinaria);
+        tvCulinaria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        TextView tvPlantas = (TextView) findViewById(R.id.tvPlantas);
+        tvPlantas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        TextView tvPovosNativos = (TextView) findViewById(R.id.tvPovosNativos);
+        tvPovosNativos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
