7d3b54d7715c71215b93e21650f47ae21cdc84e0
Fillipe Cordeiro <fillipe@androidpro.com.br>
2016-08-23 20:11:50 -0300

    Etapa 13

--- app/src/main/java/br/com/androidpro/tupicionario/BichosActivity.java
+++ app/src/main/java/br/com/androidpro/tupicionario/BichosActivity.java
@@ -2,7 +2,10 @@ package br.com.androidpro.tupicionario;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,7 +17,7 @@ public class BichosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bichos);
 
         String[] bichosArray = getResources().getStringArray(R.array.bichos);
-        String[] bichosDescArray = getResources().getStringArray(R.array.bichos_desc);
+        final String[] bichosDescArray = getResources().getStringArray(R.array.bichos_desc);
 
         ArrayList<Item> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
@@ -28,6 +31,13 @@ public class BichosActivity extends AppCompatActivity {
         ItemAdapter adapter = new ItemAdapter(this, list, R.color.bichos_categoria);
 
         ListView listView = (ListView) findViewById(R.id.rootBichos);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String desc = bichosDescArray[position];
+                Toast.makeText(BichosActivity.this, desc, Toast.LENGTH_LONG).show();
+            }
+        });
         listView.setAdapter(adapter);
     }
 }
